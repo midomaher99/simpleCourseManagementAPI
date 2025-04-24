5df73a3705732a8826caa62b5208adbda58dc213
unknown <mohamed.mostafa.maher.999@gmail.com>
2025-04-24 20:00:12 +0200

    exiplicitly connect to db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app = require(`./app`)
 
 const db = process.env.DATABASE
-mongoose.connect(db).then(() => { console.log("DB connected") })
+mongoose.connect("mongodb+srv://momaher190799:tOCZVt6y0UgBpCh0@cluster0.7bbs7mp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => { console.log("DB connected") })
 
 const server = app.listen(3000, () => {
     console.log(`app is running on port:3000`)
